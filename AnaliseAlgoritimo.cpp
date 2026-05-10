25d55b5584ed369f10ec2921959d1aa393cad24a
unknown <gabrielmedeirosn@gmail.com>
2026-05-10 18:59:23 -0300

    Correcao

--- AnaliseAlgoritimo.cpp
+++ AnaliseAlgoritimo.cpp
@@ -26,11 +26,9 @@ int BuscaSequencial (int lista[], int tam, int chave){
     for (int i = 0; i < tam; i++){
         if(lista[i] == chave){
             return i; //retorna a posicao no array
-        }
-        else {       
-            cout << "Chave nao encontrada" << endl;
-            return -1;
-        }
+        }      
+        cout << "Chave nao encontrada" << endl;
+        return -1;
     }
 }
 
@@ -44,11 +42,9 @@ int buscaBinaria(int lista[], int tam, int chave) {
         if (lista[meio] == chave){
             return meio;
         }
-
         else if (chave > lista[meio]){
             inicio = meio + 1;
         }
-
         else {
             fim = meio - 1;
         }
@@ -77,32 +73,27 @@ double calcularCrescimento (double tempo1, double tempo2, double tempo3, int tam
     double mediaTempoTotal = (mediaTempo1 + mediaTempo2) / 2;
     
     int mediaCrescTam1 = tamanho2/tamanho1;
-    int mediaCrescTam2 = tamanho3/tamanho1;
-    int mediaCrescTamTotal = (double)(mediaCrescTam1+mediaCrescTam2) / 2;
+    int mediaCrescTam2 = tamanho3/tamanho2;
+    double mediaCrescTamTotal = (double)(mediaCrescTam1+mediaCrescTam2) / 2;
 
-    return log(mediaTempoTotal) / log(mediaCrescTamTotal); //relaciona o crescimento do tempo ao crescimento da entrada. Log usado para descobrir o expoente, como consequencia complexidade.
+    return log(mediaTempoTotal) / log(mediaCrescTamTotal); //relaciona o crescimento do tempo ao crescimento da entrada. Log usado para descobrir o expoente, estima o expoente de crescimento da complexidade
 }
 
 string ClassComplexidade(double expoente){
     if(expoente < 0.5){
         return "O(log n)";
     }
-
     else if(expoente < 1.2){
         return "O(n)";
     }
-
     else if(expoente < 1.5){
         return "O(n log n)";
     }
-
     else if(expoente < 2.5){
         return "O(n^2)";
     }
-
     else if(expoente < 3.5){
         return "O(n^3)";
     }
-
     return "Complexidade maior";
 }
